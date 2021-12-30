137addd1374f05fcbc2f4780a52cc0dadbfc647e
Serhii Yatsyna <yatsyna.sergey@gmail.com>
2021-12-30 11:03:10 +0200

    ramove static swagger

--- server/server/documentation/open-api-documentation/index.ts
+++ server/server/documentation/open-api-documentation/index.ts
@@ -2,6 +2,7 @@ import forEach from "lodash/forEach";
 import express, { Express, Request, Response } from "express";
 import parser, { OpenApiConfig } from "./parser";
 import { getPathSwagger, getOpenApiSchema } from "./baseSwagger";
+import swaggerUi from "swagger-ui-express";
 
 export const getOpenApiSchemaJson = async (
   app: Express,
@@ -27,6 +28,26 @@ export const getOpenApiSchemaRouter = (app: Express, config: OpenApiConfig) => {
     respSwagger = schema;
   });
   const router = express.Router();
+  router.get("/openapi/schema.json", (req: Request, res: Response) => {
+    res.send(respSwagger);
+  });
+  return router;
+};
+
+export const getSwaggerUiRouter = (app: Express, config: OpenApiConfig) => {
+  let respSwagger = {};
+  void getOpenApiSchemaJson(app, config).then((schema) => {
+    respSwagger = schema;
+  });
+
+  const router = express.Router();
+
+  router.use("/docs", swaggerUi.serve, async (req: Request, res: Response) => {
+    return res.send(
+      swaggerUi.generateHTML(respSwagger)
+    );
+  });
+
   router.get("/openapi/schema.json", (req: Request, res: Response) => {
     res.send(respSwagger);
   });
