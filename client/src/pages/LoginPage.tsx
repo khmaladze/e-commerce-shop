1c46988c820b8104579516eac4a57793ec05350b
khmaladze <khmaladze.g.7@gmail.com>
2022-01-06 13:53:13 +0400

    small update

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useState, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { UserContext } from "../App";
+import { serverUrl, UserContext } from "../App";
 import { toast } from "react-toastify";
-import { postUserLogin } from "./ApiClient";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 toast.configure();
 
 export const LoginPage: FC = () => {
@@ -11,34 +12,48 @@ export const LoginPage: FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const PostData = async () => {
-    if (email.length > 1 && password.length > 7) {
-      try {
-        const res = await postUserLogin(email, password);
-        console.log('postUserLogin res.status ', res.status)
-        history.push("/");
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        toast.success("user log in successfully");
-        if (res.data.token) {
-          localStorage.setItem("jwt", res.data.token);
-        }
-        if (res.data.user) {
-          localStorage.setItem("user", JSON.stringify(res.data.user));
-        }
+  interface UserLogin {
+    email: string;
+    userPassword: string;
+  }
 
-        if (res.data.shop) {
-          localStorage.setItem("shop", JSON.stringify(res.data.shop));
-        }
-        dispatch({ type: "USER", payload: res.data.user });
-      } catch (error: any) {
-        if (error.response.data) {
-          for (let errDetail of error.response.data.detail) {
-            toast.warn(errDetail.message);
+  const PostData = () => {
+    if (email.length > 1 && password.length > 7) {
+      const postUserLogin = async () => {
+        try {
+          const userLogin: UserLogin = {
+            email,
+            userPassword: password,
+          };
+          const res = await axios.post(
+            `${serverUrl}/api/auth/login`,
+            userLogin
+          );
+          if (res.status == 200) {
+            history.push("/");
+            window.scrollTo({ top: 0, behavior: "smooth" });
+            toast.success("user log in successfully");
+            if (res.data.token != undefined) {
+              localStorage.setItem("jwt", res.data.token);
+            }
+            if (res.data.user != undefined) {
+              localStorage.setItem("user", JSON.stringify(res.data.user));
+            }
+
+            if (res.data.shop) {
+              localStorage.setItem("shop", JSON.stringify(res.data.shop));
+            }
+            dispatch({ type: "USER", payload: res.data.user });
           }
-        } else {
-          toast.warn("Please Use Valid Credentials");
+        } catch (error: any) {
+          if (error.response.data) {
+            toast.warn(error.response.data.detail[0].message);
+          } else {
+            toast.warn("Please Use Valid Credentials");
+          }
         }
-      }
+      };
+      postUserLogin();
     } else {
       toast.warn("Please Add All the fields and use valid credentials");
     }
