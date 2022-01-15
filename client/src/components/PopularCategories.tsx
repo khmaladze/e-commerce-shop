2d377b57f063f6c11f15319edfbb1b09dae57843
khmaladze <khmaladze.g.7@gmail.com>
2022-01-15 13:37:31 +0400

    main page looks better some style fix

--- client/src/components/PopularCategories.tsx
+++ client/src/components/PopularCategories.tsx
@@ -4,43 +4,46 @@ import img2 from "../assets/category2.jpg";
 import img3 from "../assets/category3.jpg";
 import img4 from "../assets/category4.jpg";
 import img5 from "../assets/category5.jpg";
+import { Fade } from "react-awesome-reveal";
 
 export const PopularCategories: FC = () => {
   return (
     <div>
       <h1 className="container__shop__text">Popular Categories</h1>
-      <div className="container__shop">
-        <div
-          className="shop__container"
-          style={{
-            backgroundImage: `url(${img1})`,
-          }}
-        ></div>
-        <div
-          className="shop__container"
-          style={{
-            backgroundImage: `url(${img2})`,
-          }}
-        ></div>
-        <div
-          className="shop__container"
-          style={{
-            backgroundImage: `url(${img3})`,
-          }}
-        ></div>
-        <div
-          className="shop__container"
-          style={{
-            backgroundImage: `url(${img4})`,
-          }}
-        ></div>
-        <div
-          className="shop__container"
-          style={{
-            backgroundImage: `url(${img5})`,
-          }}
-        ></div>
-      </div>
+      <Fade cascade>
+        <div className="container__shop">
+          <div
+            className="shop__container"
+            style={{
+              backgroundImage: `url(${img1})`,
+            }}
+          ></div>
+          <div
+            className="shop__container"
+            style={{
+              backgroundImage: `url(${img2})`,
+            }}
+          ></div>
+          <div
+            className="shop__container"
+            style={{
+              backgroundImage: `url(${img3})`,
+            }}
+          ></div>
+          <div
+            className="shop__container"
+            style={{
+              backgroundImage: `url(${img4})`,
+            }}
+          ></div>
+          <div
+            className="shop__container"
+            style={{
+              backgroundImage: `url(${img5})`,
+            }}
+          ></div>
+        </div>
+      </Fade>
     </div>
   );
 };
--- client/src/components/PopularShops.tsx
+++ client/src/components/PopularShops.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { FC, useState, useEffect } from "react";
+import { Fade } from "react-awesome-reveal";
 
 export const PopularShops: FC = () => {
   const [shop, setShop] = useState<any>([]);
@@ -7,68 +8,73 @@ export const PopularShops: FC = () => {
     const getShops = async () => {
       try {
         const res = await axios.get(`/api/shop`);
+        setShop(res.data.shop);
+        console.log(res);
       } catch (error) {
         console.log(error);
       }
     };
     getShops();
+    console.log(shop);
   }, []);
   return (
     <div>
       <h1 className="container__shop__text">Popular Shops</h1>
-      <div className="container__shop">
-        {shop.length == 5 ? (
-          shop.slice(0, 5).map((item: any) => {
-            return (
+      <Fade cascade>
+        <div className="container__shop">
+          {shop ? (
+            shop.slice(0, 5).map((item: any) => {
+              return (
+                <div
+                  key={item.shop_id}
+                  className="shop__container"
+                  style={{
+                    backgroundImage: `url(${item.shop_image})`,
+                  }}
+                ></div>
+              );
+            })
+          ) : (
+            <div className="container__shop">
               <div
-                key={item.shop_id}
                 className="shop__container"
                 style={{
-                  backgroundImage: `url(${item.shop_image})`,
+                  backgroundImage:
+                    "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
                 }}
               ></div>
-            );
-          })
-        ) : (
-          <div className="container__shop">
-            <div
-              className="shop__container"
-              style={{
-                backgroundImage:
-                  "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
-              }}
-            ></div>
-            <div
-              className="shop__container"
-              style={{
-                backgroundImage:
-                  "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
-              }}
-            ></div>
-            <div
-              className="shop__container"
-              style={{
-                backgroundImage:
-                  "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
-              }}
-            ></div>
-            <div
-              className="shop__container"
-              style={{
-                backgroundImage:
-                  "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
-              }}
-            ></div>
-            <div
-              className="shop__container"
-              style={{
-                backgroundImage:
-                  "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
-              }}
-            ></div>
-          </div>
-        )}
-      </div>
+              <div
+                className="shop__container"
+                style={{
+                  backgroundImage:
+                    "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
+                }}
+              ></div>
+              <div
+                className="shop__container"
+                style={{
+                  backgroundImage:
+                    "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
+                }}
+              ></div>
+              <div
+                className="shop__container"
+                style={{
+                  backgroundImage:
+                    "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
+                }}
+              ></div>
+              <div
+                className="shop__container"
+                style={{
+                  backgroundImage:
+                    "url(https://res.cloudinary.com/dtlhyd02w/image/upload/v1638523630/frdmwjc5jtxv0eobisd0.png)",
+                }}
+              ></div>
+            </div>
+          )}
+        </div>
+      </Fade>
     </div>
   );
 };
